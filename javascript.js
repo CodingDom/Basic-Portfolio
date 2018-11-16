160582568ec0250fcf0b7371af791fd4c667f311
Dom <dominicsmith1021@gmail.com>
2018-11-16 15:01:22 -0500

    Fixing up carousel

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// My carousel galleries
 var galleries = {
     first:["abstract.jpg","abstract2.jpg","abstract3.jpg"],
     second:["abstract4.jpg","abstract5.jpg","cloudy.png"],
@@ -6,20 +7,25 @@ var galleries = {
     fifth:["bear.jpg","wolf.jpg","tiger.jpg"]
 };
 
+//The div containing carousel items
 var container = getElem('container');
 
+//Grabbing all children of the container and creating the carousels
 for (var i = 0; i < container.children.length; i++) {
     carousel(container.children[i].children[0],container.children[i].children[2],galleries[container.children[i].getAttribute('id')]);
 };
 
 
 function carousel(gallery,static,images) {
-    console.log(gallery,static,images);
+    //console.log(gallery,static,images);
+
+    //To fix the css after the carousel pauses
     var originStyle = gallery.getAttribute('style');
 
+    //The carousel's current frame
     var num = 0;
 
-    gallery.addEventListener('webkitTransitionEnd', function( event ) { 
+    var moveCarousel = function( event ) { 
         var op = window.getComputedStyle(gallery).opacity;
         if (op == 0.1 || op == 0.5) {
             gallery.src = "assets/images/"+images[num];
@@ -38,8 +44,11 @@ function carousel(gallery,static,images) {
                 gallery.setAttribute('style',originStyle);
             }
         }  
-    }, false );
+    };
 
+    gallery.addEventListener('webkitTransitionEnd', function( event ) {moveCarousel();}, false );
+    gallery.addEventListener('mozTransitionEnd', function( event ) {moveCarousel();}, false );
+    gallery.addEventListener('oTransitionEnd', function( event ) {moveCarousel();}, false );
 };
 
 function getElem(id) {
