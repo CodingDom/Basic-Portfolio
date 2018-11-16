520c5a1d76b9dabf774ca925d9a832ad26ad4ef5
Dom <dominicsmith1021@gmail.com>
2018-11-16 11:20:50 -0500

    Makeshift carousel

---
+++ javascript.js
@@ -0,0 +1,47 @@
+var galleries = {
+    first:["abstract.jpg","abstract2.jpg","abstract3.jpg"],
+    second:["abstract4.jpg","abstract5.jpg","cloudy.png"],
+    third:["fish.jpg","idk.jpg","diamond.jpg"],
+    fourth:["football.jpg","basketball.jpg","ball.jpg"],
+    fifth:["bear.jpg","wolf.jpg","tiger.jpg"]
+};
+
+var container = getElem('container');
+
+for (var i = 0; i < container.children.length; i++) {
+    carousel(container.children[i].children[0],container.children[i].children[2],galleries[container.children[i].getAttribute('id')]);
+};
+
+
+function carousel(gallery,static,images) {
+    console.log(gallery,static,images);
+    var originStyle = gallery.getAttribute('style');
+
+    var num = 0;
+
+    gallery.addEventListener('webkitTransitionEnd', function( event ) { 
+        var op = window.getComputedStyle(gallery).opacity;
+        if (op == 0.1 || op == 0.5) {
+            gallery.src = "assets/images/"+images[num];
+            num=num+1;
+            if (num > images.length-1) {
+                num = 0;
+            }
+            gallery.style.opacity = 1;
+        }    
+        else {
+            static.src = "assets/images/"+images[num];
+            if (window.getComputedStyle(gallery.parentElement).boxShadow != "none") {
+                gallery.style.opacity = 0.1;
+            }
+            else {
+                gallery.setAttribute('style',originStyle);
+            }
+        }  
+    }, false );
+
+};
+
+function getElem(id) {
+    return document.getElementById(id);
+};
